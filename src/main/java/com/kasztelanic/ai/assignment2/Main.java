50b4335290bd12a2bb313799642295faae43903d
Kacper <kcpr51@hotmail.com>
2018-04-05 12:21:49 +0200

    Add Apache Commons CLI dependency

--- src/main/java/com/kasztelanic/ai/assignment2/Main.java
+++ src/main/java/com/kasztelanic/ai/assignment2/Main.java
@@ -1,11 +1,14 @@
 package com.kasztelanic.ai.assignment2;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 import com.kasztelanic.ai.assignment2.common.Report;
 import com.kasztelanic.ai.assignment2.nqueens.NQueensBacktrackingSolver;
 import com.kasztelanic.ai.assignment2.nqueens.NQueensForwardCheckingSolver;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void temp() {
 		int n = 8;
 		NQueensBacktrackingSolver sb = new NQueensBacktrackingSolver(n, false);
 		NQueensForwardCheckingSolver sf = new NQueensForwardCheckingSolver(n, false);
@@ -15,5 +18,13 @@ public class Main {
 		System.out.println(ssb.getSolution());
 		System.out.println(ssf);
 		System.out.println(ssf.getSolution());
+	}
+
+	public static void main(String[] args) {
+		Option help = new Option("help", "print this message");
+Option problem = 		
+		Options options = new Options();
+		options.addOption(help);
+
 	}
 }
