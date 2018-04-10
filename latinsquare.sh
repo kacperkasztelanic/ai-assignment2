7fd6748219f2bf29b5ebe46dfc862f0a605c4430
Kacper <kcpr51@hotmail.com>
2018-04-10 21:56:55 +0200

    Running scripts and results

---
+++ latinsquare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+for i in {1..19}
+do
+   >&2 echo BackTracking first solution: $i
+   java -jar target/csp.jar -p=LS -m=B -s=$i
+done
+
+for i in {1..19}
+do
+   >&2 echo ForwardChecking first solution: $i
+   java -jar target/csp.jar -p=LS -m=F -s=$i
+done
+
+for i in {1..5}
+do
+   >&2 echo BackTracking all solutions: $i
+   java -jar target/csp.jar -p=LS -m=B -s=$i -a
+done
+
+for i in {1..5}
+do
+   >&2 echo ForwardChecking all solutions: $i
+   java -jar target/csp.jar -p=LS -m=F -s=$i -a
+done
