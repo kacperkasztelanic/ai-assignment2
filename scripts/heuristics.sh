41d6fdde409352347dfc0d47743c12665659ac6c
Kacper <kcpr51@hotmail.com>
2018-05-15 09:55:41 +0200

    Fix paths in scripts

--- scripts/heuristics.sh
+++ scripts/heuristics.sh
@@ -2,36 +2,36 @@
 for i in {1..35}
 do
    >&2 echo BackTracking both first solution: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -H=var -H=val
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -H=var -H=val
 done
 
 for i in {1..14}
 do
    >&2 echo BackTracking both all solutions: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -a -H=var -H=val
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -a -H=var -H=val
 done
 
 for i in {1..29}
 do
    >&2 echo BackTracking var first solution: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -H=var
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -H=var
 done
 
 for i in {1..14}
 do
    >&2 echo BackTracking var all solutions: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -a -H=var
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -a -H=var
 done
 
 for i in {1..75}
 do
    >&2 echo BackTracking val first solution: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -H=val
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -H=val
 done
 
 for i in {1..14}
 do
    >&2 echo BackTracking val all solutions: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -a -H=val
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -a -H=val
 done
 
--- scripts/nqueens.sh
+++ scripts/nqueens.sh
@@ -2,23 +2,23 @@
 for i in {1..32}
 do
    >&2 echo BackTracking first solution: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i
 done
 
 for i in {1..32}
 do
    >&2 echo ForwardChecking first solution: $i
-   java -jar target/csp.jar -p=NQ -m=F -s=$i
+   java -jar ../target/csp.jar -p=NQ -m=F -s=$i
 done
 
 for i in {1..15}
 do
    >&2 echo BackTracking all solutions: $i
-   java -jar target/csp.jar -p=NQ -m=B -s=$i -a
+   java -jar ../target/csp.jar -p=NQ -m=B -s=$i -a
 done
 
 for i in {1..15}
 do
    >&2 echo ForwardChecking all solutions: $i
-   java -jar target/csp.jar -p=NQ -m=F -s=$i -a
+   java -jar ../target/csp.jar -p=NQ -m=F -s=$i -a
 done
